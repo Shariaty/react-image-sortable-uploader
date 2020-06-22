cb44a9028cd7a5a88132e949653501e2c1540270
ala <ala.sharyaty@gmail.com>
2020-06-22 08:19:06 +0430

    add warning untill completion

--- example/src/App.js
+++ example/src/App.js
@@ -4,17 +4,19 @@ import { SortableUploader } from 'react-image-sortable-uploader'
 import 'react-image-sortable-uploader/dist/index.css'
 
 const App = () => {
-  return <SortableUploader
-          header='Header Text'
-          title='Title Text'
-          subTitle="Subtitle Text"
-          urlUpload='https://test.com/image/upload'
-          urlDelete='https://test.com/image/remove'
-          editMode={true}
-          Identifier={555}
-          // onEdit={(sortedData) => console.log(sortedData)}
-          onSort={(sortedData) => console.log(sortedData)}
-          theme='blue'/>
+  return <div style={{padding: 50}}>
+            <SortableUploader
+              header='Photo Gallery'
+              title='Upload at least 1 photo'
+              subTitle="You`ll also be able to upload more after registration"
+              urlUpload='https://test.com/image/upload'
+              urlDelete='https://test.com/image/remove'
+              editMode={true}
+              Identifier={555}
+              onEdit={(editData) => console.log(editData)}
+              onSort={(sortedData) => console.log(sortedData)}
+              theme='blue'/>
+        </div>
 }
 
 export default App
